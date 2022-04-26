e4937f7180994070d4131a71fb4d681262ede2e0
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2022-04-26 13:52:27 +0900

    Added new dsl.

---
+++ nodejs.groovy
@@ -0,0 +1,15 @@
+job('NodeJS Job') {
+    scm {
+        git('https://github.com/TetsujiFurukawa/testcafe-ci-example.git', 'master') {  node ->
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@example.com')
+        }
+    }
+    wrappers {
+        nodejs('node_latest')
+    }
+    steps {
+        shell("npm install")
+        shell("npx testcafe chrome:headless tests/test.js -r xunit:report.xml")
+    }
+}

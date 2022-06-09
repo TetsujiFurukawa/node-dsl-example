3fb67c70857042a9c58fac2a6b1c967402fdf2d9
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2022-06-09 14:48:23 +0900

    Edit publishers.

--- nodejs.groovy
+++ nodejs.groovy
@@ -8,8 +8,14 @@ job('testcafe-dsl-example') {
     steps {
         shell("npm install")
         shell("npx testcafe chrome:headless tests/test.js -s takeOnFails=true --video artifacts/videos --video-options ,failedOnly=true -r jenkins:report.xml")
-    }
+    } 
     publishers {
-        archiveJunit('report.xml')
+        post {
+            always {
+                junit keepLongStdio: true,
+                testDataPublishers: [[$class: 'TestCafePublisher']],
+                testResults: 'report.xml'
+            }
+        }
     }
 }
